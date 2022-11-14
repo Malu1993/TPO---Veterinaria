391ae23de491ededc8baf8d108aff77257ec4212
Maria Laura Sfrazzetta <113934001+Malu1993@users.noreply.github.com>
2022-11-14 14:20:14 -0300

    Delete js directory

--- js/app.js
+++
@@ -1,9 +0,0 @@
-addEventListener('DOMContentLoaded', () => {
-    const boton = document.querySelector('.boton')
-    if(boton){
-        boton.addEventListener('click', () => {
-            const menu = document.querySelector('.menu')
-            menu.classList.toggle('mostrar')
-        })
-    }
-})
